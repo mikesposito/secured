6e3ca14e0ae4baa08323bff221cece98d56f3e4c
Michele Esposito <34438276+mikesposito@users.noreply.github.com>
2023-11-30 23:31:35 +0100

    chore: add benches (#11)

---
+++ benches/src/chacha20.rs
@@ -0,0 +1,29 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use secured_cipher::chacha20::ChaChaStream;
+
+const KB: usize = 1024;
+
+fn bench(c: &mut Criterion) {
+  let mut group = c.benchmark_group("ChaChaStream");
+
+  for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
+    let key = [0u8; 32];
+    let iv = [1u8; 8];
+
+    group.throughput(Throughput::Bytes(*size as u64));
+
+    group.bench_with_input(BenchmarkId::new("new", size), size, |b, &_size| {
+      b.iter(|| ChaChaStream::new(key, iv));
+    });
+
+    let mut stream = ChaChaStream::new(key, iv);
+    group.bench_with_input(BenchmarkId::new("process", size), size, |b, &_size| {
+      b.iter(|| stream.process(&vec![0u8; *size]));
+    });
+  }
+
+  group.finish();
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
